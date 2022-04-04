dada02fc5084da35d27f9b00ac96d6d467d48a0e
kayla <kay.ann2000@gmail.com>
2022-04-04 12:00:38 -0600

    commit tree 47fa00904999462bae8b9972487deb6c9932ab9b

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
-
+import TestComponent from "./TestComponent";
 // Add your code own within the return statement
 function App() {
   return (
+    // const header = document.getElementById('h1') 
+    // <h1>Now</h1>;
+    
     <div className="App">
       <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
       <p className="App-intro">
@@ -14,6 +17,7 @@ function App() {
         self-contained chunks of JSX.
       </p>
       <ExampleComponent />
+      <TestComponent/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import { doc } from "prettier";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
+const header = document.getElementById('h1') 
+h1.textContent = 'Now'
 
 ReactDOM.render(<App />, document.getElementById("root"));
